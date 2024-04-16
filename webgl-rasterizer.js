57d15455ff0d81630c44c7a7051979cec4677eb6
Camden Murphy <camdenmurphy2001@gmail.com>
2024-04-16 01:05:34 -0500

    added initializeShader() and loadShader() funcitons to webgl-rasterizer.js

--- webgl-rasterizer.js
+++ webgl-rasterizer.js
@@ -16,6 +16,68 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Clear the color buffer to apply the clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Vertex shader
+    const vsSource = `
+        attribute vec4 aVertexPosition;
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+        void main() {
+            gl_Position = uProjectionMatrix * 
+            uModelViewMatrix * aVertexPosition;
+        }
+    `;
+
+    // Fragment shader
+    const fsSource = `
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    `;
+
+}
+
+// Initializes the shader program
+function initializeShaders(gl, vsSource, fsSource) {
+    // Create shader program
+    const shaderProgram = gl.createProgram();
+
+    // Create shaders
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    
+    // Attatch shaders' code to memory on GPU and link them together
+    gl.attatchShader(shaderProgram, vertexShader);
+    gl.attatchShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // Throw error if shader program fails to create
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        throw gl.getProgramInfoLog(shaderProgram);
+    }
+
+    return shaderProgram;
+}
+
+// Creates a shader, given a type, and compiles it
+function loadShader(gl, type, source) {
+    // Create a new shader
+    const shader = gl.createShader(type);
+
+    // Send the shader's source code to the shader
+    gl.shaderSource(shader, source);
+
+    // Compile
+    gl.compileShader(shader);
+
+    // Error checking
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(`Error compiling the shaders: ${gl.getShaderInfoLog(shader)}`);
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
 }
 
 main();
